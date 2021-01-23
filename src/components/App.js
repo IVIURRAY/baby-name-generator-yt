d60373c7854a90c618154bf7bcd29b12dfc4c9ab
IVIURRAY <haydn.murray1@yahoo.co.uk>
2021-01-23 17:00:03 +0000

    touch up

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,6 @@ import Emoji from 'react-emoji-render';
 import NameContainer from '../containers/NameContainer';
 import ButtonContainer from '../containers/ButtonContainer';
 import './App.css';
-import Emoji from 'react-emoji-render';
-import Names from './names'
-import 'antd/dist/antd.css';
 
 function App() {
   return (
